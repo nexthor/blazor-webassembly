699e0b9dcc4360e40414f207a00cd119c8917dc2
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-01-25 19:50:09 -0600

    RenderFragments, Partial Classes, Lifecycle methods and Logging

---
+++ BlazorProducts.Client/Components/Home.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
+
+namespace BlazorProducts.Client.Components
+{
+    public partial class Home
+    {
+        // EditorRequired make the parameter required for the parent component
+        [EditorRequired]
+        [Parameter]
+        public string? Title { get; set; }
+
+        // set addional paramenter as a collection
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+        [CascadingParameter(Name = "HeadingColor")]
+        public string? Color { get; set; }
+        [Parameter]
+        public RenderFragment? NewContentProperty { get; set; }
+    }
+}
--- BlazorProducts.Client/Program.cs
+++ BlazorProducts.Client/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// to show all the logging levels
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
