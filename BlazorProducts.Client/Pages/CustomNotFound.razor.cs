801d233c6a3765f76e1e5fff791b41ef0c5a5f59
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-01-26 19:27:00 -0600

    custom not found page and routing

---
+++ BlazorProducts.Client/Pages/CustomNotFound.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Xml.XPath;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class CustomNotFound
+    {
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+        public void NavigateToHome()
+        {
+            NavigationManager?.NavigateTo("/");
+        }
+    }
+}
