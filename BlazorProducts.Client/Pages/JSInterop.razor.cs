e04eeacd61a5b07e9cfefe5cbd97f34ae30367f9
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-02-21 18:40:18 -0600

    jsinteropration with and without returning any values from js file

---
+++ BlazorProducts.Client/Pages/JSInterop.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class JSInterop
+    {
+        [Inject]
+        public IJSRuntime? JSRuntime { get; set; }
+
+        private IJSObjectReference? _jsModule;
+        private string? _registrationResult;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _jsModule = await JSRuntime!.InvokeAsync<IJSObjectReference>("import", "./js/jsExamples.js");
+        }
+
+        public async Task ShowAlertWindow()
+        {
+            //await _jsModule!.InvokeVoidAsync("showAlert", "JS Function call");
+            await _jsModule!.InvokeVoidAsync("showAlertObject", new { Name = "Nestor", Age = 45 });
+        }
+
+        private async Task RegisterEmail()
+        {
+            _registrationResult = await _jsModule!.InvokeAsync<string>("emailRegistration", "Please provide your email");
+        }
+    }
+}
---
+++ BlazorProducts.Client/wwwroot/js/jsExamples.js
@@ -0,0 +1,19 @@
+﻿export function showAlert(message) {
+    alert(message);
+}
+
+export function showAlertObject(person) {
+    const message = `this person is called ${person.name} and is ${person.age} years old`;
+    alert(message);
+}
+
+export function emailRegistration(message) {
+    const result = prompt(message);
+
+    if (!result)
+        return 'Please provide an email to register';
+
+    const returnMessage = `Thanks, your email ${result} has been registered correctly`;
+
+    return returnMessage;
+}
