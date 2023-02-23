97520b12d0511801563aaef711f6005a4b67d4dc
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-02-22 20:47:04 -0600

    interact with c# methods in javascript

---
+++ BlazorProducts.Client/Pages/JSInterop2.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class JSInterop2
+    {
+        [JSInvokable]
+        public static string CalculateSquareRoot(int number)
+        {
+            var result = Math.Sqrt(number);
+
+            return $"The square root of {number} is {result}";
+        }
+    }
+}
---
+++ BlazorProducts.Client/wwwroot/js/jsExamples2.js
@@ -0,0 +1,17 @@
+﻿var jsFunctions = {};
+
+jsFunctions.calculateSquareRoot = function () {
+    const number = prompt("Enter your number");
+
+    // 3 parameters:
+    // - assembly name
+    // - name of method
+    // - parameter
+    // the result is a promise
+    DotNet.invokeMethodAsync("BlazorProducts.Client", "CalculateSquareRoot", parseInt(number))
+        .then(result => {
+            var el = document.getElementById("string-result");
+
+            el.innerHTML = result;
+        })
+}
